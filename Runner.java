780211fde36b26fe60a691af125fd0d7bd86452f
Rayonnahill1 <hill_rayonna@students.columbusstate.edu>
2025-02-27 21:56:35 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,38 +1,49 @@
-//this class tests all methods in the Calc class
-//the Calc class exposes the following methods:
-//add, subtract, multiply, divide
-//user is prompted for input
 import java.util.Scanner;
-public class Runner {
-    public static void main(String[] args){
-        //instantiate a Calc object
-        Calc myCalculator = new Calc();
-        //get user input for two numbers
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the first number: ");
-        double n1 = scan.nextDouble();
-        System.out.println("Please enter the second number: ");
-        double n2 = scan.nextDouble();
 
-        //pass the numbers to the Calc object
+public class Runner {
+    public static void main(String[] args) {
+        // Instantiate a Calc object
+        Calc myCalculator = new Calc();
+        // Create a Scanner object for user input
+        Scanner scan = new Scanner(System.in);
+
+        // Get valid user input for two numbers
+        double n1 = getValidNumber(scan, " User,please enter the first number: ");
+        double n2 = getValidNumber(scan, "User, please enter the second number: ");
+
+        // Pass the numbers to the Calc object
         myCalculator.setNum1(n1);
         myCalculator.setNum2(n2);
 
-        //output from Calc instance
+        // Output from Calc instance
         System.out.println(myCalculator);
 
-        //examining the instance private data fields by calling get methods
+        // Examining the instance private data fields by calling get methods
         System.out.println("Calling num1 get method: " + myCalculator.getNum1());
         System.out.println("Calling num2 get method: " + myCalculator.getNum2());
 
-        //calling Calc methods directly
-        //can use a local variable for sum if you need that value for another calculation
+        // Calling Calc methods directly
         double sum = myCalculator.add();
         System.out.println("The sum is: " + sum);
-
-        //or if you don't, then just display the difference, product and quotient
         System.out.println("The difference is: " + myCalculator.subtract());
         System.out.println("The product is: " + myCalculator.multiply());
         System.out.println("The quotient is: " + myCalculator.divide());
+    }
+
+    // Method to get valid numeric input from the user
+    private static double getValidNumber(Scanner scan, String prompt) {
+        double number;
+        while (true) {
+            System.out.print(prompt);
+            if (scan.hasNextDouble()) {
+                number = scan.nextDouble();
+                scan.nextLine(); //  newline character
+                break;
+            } else {
+                System.out.println("This is incorrect. Please enter a valid number.");
+                scan.next(); // Clear the invalid input
+            }
+        }
+        return number;
     }
 }
